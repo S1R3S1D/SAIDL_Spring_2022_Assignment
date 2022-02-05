d151a85d175100f24158e2deb66d2055145fda4c
S1R3S1D <81525963+S1R3S1D@users.noreply.github.com>
2022-02-05 16:55:32 +0530

    Update Helper_Functions.py

--- Bayesian_Neural_Networks/Helper_Functions.py
+++ Bayesian_Neural_Networks/Helper_Functions.py
@@ -5,21 +5,27 @@ import matplotlib.pyplot as plt
 from tqdm import tqdm
 import pandas as pd
 
+#Finds the mean of data X
+
 def mean(X):
   return float(sum(X))/float(len(X))
 
+#Finds the standard deviation of data X
 def std_dev(X):
   mu = mean(X)
   return (sum([(float(x)-mu)**2 for x in X])/(float(len(X))-1))**(1/2)
 
+#Finds the value of x over the normal distribution with mean mu and standard deviation sigma
 def normal(x, mu, sigma):
   num = np.exp((-1/2)*((x-mu)/sigma)**2)
   denom = sigma*math.sqrt(2*math.pi)
   return num/denom
 
+#The sigmoid activation function
 def sigmoid(x):
   return 1/(1+np.exp(-x))
 
+#A binary step activation function
 def binary_step(x, inflection_point):
   ret = []
   for i in x:
@@ -30,12 +36,13 @@ def binary_step(x, inflection_point):
 
   return np.array(ret)
 
+#Returns the argmax of a vector in the form of a vector
 def argmax_array(x):
   i = np.argmax(x)
   rets = np.zeros_like(x)
   rets[i]=1
   return rets
-
+#Afunction to retrieve data from the given path and split it into the input and output data
 def data_retriever(path):
     df = pd.read_csv(str(path))
     df_np = np.array(df[:][:])
